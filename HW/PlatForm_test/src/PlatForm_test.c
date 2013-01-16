1b76020898e0cddece6023b2541a9ec4a5c1c7e7
Flindt <pfl@eal.dk>
2013-01-16 10:46:40 +0100

    added test runner runs one empty test in i2c that passes

--- HW/PlatForm_test/src/PlatForm_test.c
+++ HW/PlatForm_test/src/PlatForm_test.c
@@ -11,9 +11,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <platform.h>
+#include "seatest.h"
+
+
+void test_i2c (void );
+
+void all_tests( void )
+{
+        test_i2c();
+        // add new test fixtures here.
+}
+
+void my_suite_setup( void )
+{
+        //printf("I'm done before every single test in the suite\r\n");
+}
+
+void my_suite_teardown( void )
+{
+        //printf("I'm done after every single test in the suite\r\n");
+}
+
+
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+	suite_setup(my_suite_setup);
+        suite_teardown(my_suite_teardown);
+        run_tests(all_tests);
+
 	return EXIT_SUCCESS;
 }
