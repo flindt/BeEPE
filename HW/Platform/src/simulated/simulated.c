1637f4ffdae31e698bd32e9f012380982515baf9
Flindt <pfl@eal.dk>
2013-01-16 11:46:59 +0100

    added comment to simulated.c

--- HW/Platform/src/simulated/simulated.c
+++ HW/Platform/src/simulated/simulated.c
@@ -3,6 +3,18 @@
  *
  *  Created on: Jan 15, 2013
  *      Author: flindt
+ *
+ *
+ *      This file holds the simulation for the I2C bus.
+ *      It pretends to have two slave devices :
+ *              slave A :       Type:   T100
+ *                              Address: 111 ->
+ *                              Temp:   27.3 degrees
+ *              slave B :       Type:   T100
+ *                              Address: 110 ->
+ *                              Temp:   18.9 degrees
+ *
+ *
  */
 
 #include "platform.h"
